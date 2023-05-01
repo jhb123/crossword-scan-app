60d6c0e0e22df6956425d545199417ef0c6b97e5
jhb123 <jhbriggs23@gmail.com>
2023-05-01 09:22:05 +0100

    Adds the clue entry ui

--- app/src/main/java/com/example/learn_opencv/ui/PuzzleSolveViewModel.kt
+++ app/src/main/java/com/example/learn_opencv/ui/PuzzleSolveViewModel.kt
@@ -1,16 +1,20 @@
 package com.example.learn_opencv.viewModels
 
 import android.graphics.Bitmap
+import android.net.Uri
 import android.util.Log
-import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.*
 import androidx.lifecycle.*
 import com.example.learn_opencv.Clue
 import com.example.learn_opencv.Puzzle
 import com.example.learn_opencv.PuzzleData
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import com.example.learn_opencv.PuzzleRepository
 import com.example.learn_opencv.ui.PuzzleUiState
 import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.*
+import kotlin.math.max
 
 class PuzzleSolveViewModel(private val repository: PuzzleRepository,private val puzzleIdx : Int = 0): ViewModel() {
 
@@ -30,33 +34,66 @@ class PuzzleSolveViewModel(private val repository: PuzzleRepository,private val 
             initialValue = PuzzleUiState()
         )
 
-    //val active_cell = uiState.value.current_cell_coord
+//    private val _uiState = MutableStateFlow(PuzzleUiState())
 
-    private val activeCell = MutableLiveData<Pair<Int,Int>>()
-    fun getActiveCell() = activeCell
 
-    fun setActiveCell(coord : Pair<Int,Int>){
-        Log.i(TAG,"setting active cell")
-        //uiState.value.currentCellCoord = coord
-        setActiveClue(coord)
-        activeCell.postValue(coord)
+    fun getPuzzleDim() : Int {
+        var maxVal = 0
+        uiState.value.currentPuzzle.clues.forEach { s, clue ->
+            clue.clueBoxes.forEach {
+                maxVal = max(maxVal,max(it.first,it.second))
+            }
+        }
+        return maxVal+1 //the index+1 gives the maximum size!
     }
 
-    private val activeClue = MutableLiveData<Clue>()
-    fun getActiveClue() = activeClue
 
-    fun setActiveClue(coord : Pair<Int,Int>){
-        Log.i(TAG,"setting active clue")
+    var currentCell by mutableStateOf(Pair<Int,Int>(0,0))
+        private set
+
+    fun updateCurrentCell(coord : Pair<Int,Int>){
+        currentCell = coord
+    }
+
+    var activeClue2 by mutableStateOf(Clue("", listOf<Pair<Int,Int>>()))
+        private set
+
+    fun updateactiveClue2(coord : Pair<Int,Int>) {
+        Log.i(TAG, "trying to update clue at $coord. Current Active clue ${activeClue2.clueName} ")
         val allClues = uiState.value.currentPuzzle.clues
-        for(clue in allClues){
-            if(clue.value.clueBoxes.contains(coord) && clue.value != activeClue.value){
-                activeClue.postValue(clue.value)
+        for (clue in allClues) {
+            if (clue.value.clueBoxes.contains(coord) && clue.value != activeClue2) {
+                Log.i(TAG, "setting active clue at $coord to ${clue.key}")
+                activeClue2 = clue.value
+                break
             }
         }
     }
 
+    //val imageList = mutableStateMapOf<Int, Uri>()
+
+    val cellLetterMap = mutableStateMapOf<Pair<Int,Int>,String>()
+
+    fun setLetter(letter: String){
+        cellLetterMap[currentCell] = letter
+        Log.i(TAG,"setting $letter for $currentCell")
+        val idx = activeClue2.clueBoxes.indexOf(currentCell)
+        if(idx+1 < activeClue2.clueBoxes.size )
+            currentCell = activeClue2.clueBoxes[idx+1]
+    }
+
+    fun delLetter(){
+        cellLetterMap[currentCell] = ""
+        Log.i(TAG,"deleting for $currentCell")
+        val idx = activeClue2.clueBoxes.indexOf(currentCell)
+        if(idx-1 >= 0 )
+            currentCell = activeClue2.clueBoxes[idx-1]
+    }
+
+
 
 }
+
 
 class PuzzleSolveViewModelFactory(private val repository: PuzzleRepository) : ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
--- app/src/main/java/com/example/learn_opencv/ui/PuzzleUiState.kt
+++ app/src/main/java/com/example/learn_opencv/ui/PuzzleUiState.kt
@@ -6,7 +6,12 @@ import com.example.learn_opencv.Puzzle
 data class PuzzleUiState (
     val currentPuzzle : Puzzle = Puzzle(),
     val name : String = "",
-    var currentCellCoord : Pair<Int,Int> = Pair(0,0),
+    var currentCellCoord : Pair<Int,Int> = Pair(-1,-1),
     val clues : Map<String, Clue> = mapOf("noname" to Clue("noname", listOf<Pair<Int,Int>>() )),
-    val current_clue : Clue = Clue("noname", listOf<Pair<Int,Int>>() )
+    var currentClue : Clue = Clue("noname", listOf<Pair<Int,Int>>() ),
+    var cellLetterMap : MutableMap<Pair<Int,Int>,String> = mutableMapOf(Pair(-1,-1) to "test")
 )
+
+data class CellUiState (
+    var text : Char
+        )
