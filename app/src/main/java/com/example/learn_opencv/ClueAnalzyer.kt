4839aabd8617f133e210a5c16c227ec7b2f9d613
jhb123 <jhbriggs23@gmail.com>
2023-05-07 18:52:08 +0100

    Adds a preview fragment with an image analyser

---
+++ app/src/main/java/com/example/learn_opencv/ClueAnalzyer.kt
@@ -0,0 +1,18 @@
+package com.example.learn_opencv
+
+import androidx.camera.core.ExperimentalGetImage
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.ImageProxy
+import com.google.mlkit.vision.common.InputImage
+
+@ExperimentalGetImage class ClueAnalzyer : ImageAnalysis.Analyzer {
+
+    override fun analyze(imageProxy: ImageProxy) {
+        val mediaImage = imageProxy.image
+        if (mediaImage != null) {
+            val image = InputImage.fromMediaImage(mediaImage, imageProxy.imageInfo.rotationDegrees)
+            // Pass image to an ML Kit Vision API
+            // ...
+        }
+    }
+}
--- app/src/main/java/com/example/learn_opencv/fragments/ClueScanFragment.kt
+++ app/src/main/java/com/example/learn_opencv/fragments/ClueScanFragment.kt
@@ -1,60 +1,175 @@
 package com.example.learn_opencv.fragments
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.learn_opencv.R
+import android.widget.Toast
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
+import androidx.compose.foundation.layout.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.platform.ViewCompositionStrategy
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.activityViewModels
+import com.example.learn_opencv.PuzzleApplication
+import com.example.learn_opencv.viewModels.CrosswordScanViewModel
+import com.example.learn_opencv.viewModels.CrosswordScanViewModelFactory
+import com.google.mlkit.vision.common.InputImage
+import java.util.concurrent.Executors
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [ClueScanFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
+private const val TAG = "ClueScanFragment"
+
 class ClueScanFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+    private val viewModel: CrosswordScanViewModel by activityViewModels{
+        CrosswordScanViewModelFactory((requireActivity().application as PuzzleApplication).repository)
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_clue_scan, container, false)
+    ): View {
+        return ComposeView(requireContext()).apply {
+            setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
+            setContent {
+
+                if (allPermissionsGranted()) {
+                    //startCamera()
+                } else {
+                    requestPermissions(
+                        REQUIRED_PERMISSIONS,
+                        REQUEST_CODE_PERMISSIONS
+                    )
+                }
+
+                Column() {
+                    CameraPreview()
+                    //Box(modifier = Modifier.size(width = 200.dp, height = 200.dp))
+                }
+
+
+
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int, permissions: Array<String>, grantResults:
+        IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (allPermissionsGranted()) {
+                //startCamera()
+            } else {
+                Toast.makeText(this.context,
+                    "Permissions not granted by the user.",
+                    Toast.LENGTH_SHORT).show()
+                activity?.finish()
+            }
+        }
+    }
+
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(
+            requireActivity().baseContext, it) == PackageManager.PERMISSION_GRANTED
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ClueScanFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ClueScanFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+
+        private const val REQUEST_CODE_PERMISSIONS = 10
+        private val REQUIRED_PERMISSIONS =
+            mutableListOf (
+                Manifest.permission.CAMERA,
+            ).apply {
+                if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.P) {
+                    add(Manifest.permission.WRITE_EXTERNAL_STORAGE)
                 }
-            }
+            }.toTypedArray()
     }
 }
+
+@Composable
+fun CameraPreview(
+    modifier: Modifier = Modifier,
+    cameraSelector: CameraSelector = CameraSelector.DEFAULT_BACK_CAMERA,
+    scaleType: PreviewView.ScaleType = PreviewView.ScaleType.FILL_CENTER,
+) {
+    val lifecycleOwner = LocalLifecycleOwner.current
+    AndroidView(
+        modifier = modifier
+            //sort of limits the size of the preview, but aspect ratio needs fixing somehow
+            .fillMaxHeight(0.2f),
+        factory = { context ->
+            val previewView = PreviewView(context).apply {
+                this.scaleType = scaleType
+                layoutParams = ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.MATCH_PARENT
+                )
+                // Preview is incorrectly scaled in Compose on some devices without this
+                implementationMode = PreviewView.ImplementationMode.COMPATIBLE
+            }
+
+
+            val cameraProviderFuture = ProcessCameraProvider.getInstance(context)
+
+            cameraProviderFuture.addListener({
+                val cameraProvider = cameraProviderFuture.get()
+
+                // Preview
+                val preview = Preview.Builder()
+                    .build()
+                    .also {
+                        it.setSurfaceProvider(previewView.surfaceProvider)
+                    }
+
+                val imageAnalysis = ImageAnalysis.Builder()
+                    .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+                    .build()
+
+                val executor = Executors.newSingleThreadExecutor()
+                imageAnalysis.setAnalyzer(executor, ImageAnalysis.Analyzer { imageProxy ->
+                    val rotationDegrees = imageProxy.imageInfo.rotationDegrees
+                    // insert your code here.
+                    Log.i(TAG,"${imageProxy.imageInfo.timestamp}")
+                    // after done, release the ImageProxy object
+                    imageProxy.close()
+                })
+
+
+
+                try {
+                    // Must unbind the use-cases before rebinding them.
+                    cameraProvider.unbindAll()
+
+                    cameraProvider.bindToLifecycle(
+                        lifecycleOwner, cameraSelector, imageAnalysis, preview
+                    )
+
+                } catch (exc: Exception) {
+                    Log.e(TAG, "Use case binding failed", exc)
+                }
+
+
+
+            }, ContextCompat.getMainExecutor(context))
+
+            previewView
+        })
+}
+
