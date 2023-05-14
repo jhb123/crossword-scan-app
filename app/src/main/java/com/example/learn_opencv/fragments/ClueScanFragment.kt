dbe83139730fd30ff3e5f0bfd8a2797ef47754c0
jhb123 <jhbriggs23@gmail.com>
2023-05-14 18:46:08 +0100

    fix roi bug

--- app/src/main/java/com/example/learn_opencv/fragments/ClueScanFragment.kt
+++ app/src/main/java/com/example/learn_opencv/fragments/ClueScanFragment.kt
@@ -288,6 +288,12 @@ class ClueScanFragment : Fragment() {
                                 if(y1 < 0){
                                     y1 = 0f
                                 }
+                                if(x2 < 0){
+                                    x2 = 0f
+                                }
+                                if(y2 < 0){
+                                    y2 = 0f
+                                }
 
                                 Log.i(TAG, "rectangle drawn using x1: $x1, x2: $x2, y1: $y1, y2: $y2")
                                 Log.i(TAG,"rectangle offset at ${Offset(x1, y1)}")
