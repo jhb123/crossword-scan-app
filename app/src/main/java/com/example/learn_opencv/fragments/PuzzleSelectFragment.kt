36f9a97a4469078103bbd808c8335b1c4997139b
jhb123 <jhbriggs23@gmail.com>
2023-05-19 07:37:21 +0100

    makes the selection screen a composable

--- app/src/main/java/com/example/learn_opencv/fragments/PuzzleSelectFragment.kt
+++ app/src/main/java/com/example/learn_opencv/fragments/PuzzleSelectFragment.kt
@@ -6,11 +6,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.ViewCompositionStrategy
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.learn_opencv.adapters.PuzzleCardAdapter
+import com.example.learn_opencv.data.PuzzleData
 import com.example.learn_opencv.databinding.FragmentPuzzleSelectBinding
+import com.example.learn_opencv.ui.puzzleSelectionScreen.puzzleSelectionComposable
 import com.example.learn_opencv.viewModels.*
 
 
@@ -20,45 +25,47 @@ class puzzleSelectFragment : Fragment() {
     private var _binding: FragmentPuzzleSelectBinding? = null
     private val binding get() = _binding!!
 
-    private val viewModel: PuzzleSelectViewModel by activityViewModels{
+    private val viewModel: PuzzleSelectViewModel by activityViewModels {
         PuzzleSelectViewModelFactory((requireActivity().application as PuzzleApplication).repository)
-    }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        Log.i(TAG,"In onCreateView")
-        // Inflate the layout for this fragment
-        _binding = FragmentPuzzleSelectBinding.inflate(inflater, container, false)
-        return binding.root
-        //return inflater.inflate(R.layout.fragment_puzzle_select, container, false)
+        return ComposeView(requireContext()).apply {
+            setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
+
+            setContent {
+                puzzleSelectionComposable(
+                    uiState = viewModel.uiState.collectAsState(),
+                    navigateToPuzzle = { }
+                )
+            }
+
+        }
     }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        Log.i(TAG,"In onViewCreated")
-
-        super.onViewCreated(view, savedInstanceState)
-
-        //val dataset = listOf<PuzzleData>()
-        val recyclerView = binding.recyclerView
-        val adapter = PuzzleCardAdapter(context)
-        recyclerView.adapter = adapter
-        recyclerView.layoutManager = LinearLayoutManager(context)
-
-        //recyclerView.addOnItemTouchListener
-
-        viewModel.allPuzzles.observe(viewLifecycleOwner, Observer{puzzles ->
-            Log.i(TAG,"adding puzzle to recycler view")
-            puzzles?.let { adapter.submitList(it) }
-
-        })
-
-    }
-
-
 }
+
+//    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+//        Log.i(TAG,"In onViewCreated")
+//
+//        super.onViewCreated(view, savedInstanceState)
+//
+//        //val dataset = listOf<PuzzleData>()
+//        val recyclerView = binding.recyclerView
+//        val adapter = PuzzleCardAdapter(context)
+//        recyclerView.adapter = adapter
+//        recyclerView.layoutManager = LinearLayoutManager(context)
+//
+//        //recyclerView.addOnItemTouchListener
+//
+//        viewModel.allPuzzles.observe(viewLifecycleOwner, Observer{puzzles ->
+//            Log.i(TAG,"adding puzzle to recycler view")
+//            puzzles?.let { adapter.submitList(it) }
+//
+//        })
+//
+//    }
+
+
---
+++ app/src/main/java/com/example/learn_opencv/ui/puzzleSelectionScreen/PuzzleSelectionUiState.kt
@@ -0,0 +1,7 @@
+package com.example.learn_opencv.ui.puzzleSelectionScreen
+
+import com.example.learn_opencv.data.PuzzleData
+
+data class PuzzleSelectionUiState(
+    val puzzles : List<PuzzleData> = listOf<PuzzleData>()
+)
